a49255d4f86516c8c07cf515e6bb4265800dce0e
Dinah <umbohsbba@yahoo.com>
2017-04-04 14:00:39 -0400

    sstudent referral is working

--- src/main/java/backpackBridge/StudentController.java
+++ src/main/java/backpackBridge/StudentController.java
@@ -4,6 +4,8 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,5 +26,8 @@ public class StudentController {
 		model.addAttribute("student", allStudents.findOne(id));
 		return "student";
 	}
-
+	 @PostMapping("/showStudent")
+	    public String studentSubmit(@ModelAttribute Student student) {
+	        return "student";
+}
 }
